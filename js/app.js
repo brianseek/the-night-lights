77894a7f0cf3eae9325572e94d80cb6fb480da4e
Brian Seek <brian.seek@civicactions.com>
2024-08-18 14:17:26 -0700

    bits and bobs, figuring out detail pages.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-new SimpleLightbox({elements: '.lb-fig a'});
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+var hpBox = new SimpleLightbox({elements: '.lb-fig a'});
+var ppBox = new SimpleLightbox({elements: '.photo-page .photo a'});
+if (viewportWidth < 450) {
+  hpBox.destroy();
+  ppBox.destroy();
+}
