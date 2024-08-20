d550265f9a95cb43bcda80bd33b4e54bb53bdbc3
Brian Seek <brian.seek@civicactions.com>
2024-08-20 14:13:05 -0700

    more features

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ if (viewportWidth < 450) {
 var encEmail = "YnJpYW4uc2VlaytuLWxAZ21haWwuY29t";
 const form = document.getElementById("contact");
 form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+
+document.addEventListener("DOMContentLoaded", function() {
+  var fontSwitch = document.getElementsByClassName('font-size-toggle')[0];
+  var body = document.getElementsByTagName('body')[0];
+
+  fontSwitch.addEventListener('click', function() {
+    if (body.classList.contains('font-size--default')) {
+      body.classList.remove('font-size--default');
+      body.classList.add('font-size--large');
+    } else {
+      body.classList.remove('font-size--large');
+      body.classList.add('font-size--default');
+    }
+  });
+});
